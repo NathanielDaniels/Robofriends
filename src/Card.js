b015d611d640b16e9bcd941ea278b19efc1e72e9
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-10-02 16:03:22 -0500

    created first component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Card = props => {
+const Card = ({ name, email, id }) => {
+  // const { name, email, id } = props;
   return (
     <div className="tc bg-light-green dib br3 ma2 grow bw2 shadow-5">
-      <img src="https://robohash.org/hgdhdcj?200x200" alt="robots" />
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
