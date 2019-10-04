12e9b771cebb3f5e6f73c6ed050a6b7cfebc614f
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-10-03 20:13:40 -0500

    created Build

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,24 +22,21 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0) {
-      return <h1 className="f1 tc green">Loading</h1>;
-    } else {
-      return (
-        <div className="tc mainContainer">
-          <h1 className="title green ttu f1">Robofriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? (
+      <h1 className="f1 tc green">Loading</h1>
+    ) : (
+      <div className="tc mainContainer">
+        <h1 className="title green ttu f1">Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
